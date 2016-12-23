13892acda8fc825b01c64a81d761dc5d98667e9c
KamaaAn <andruszczak.kamila@gmail.com>
2016-12-23 19:05:18 +0100

    first coment

---
+++ self/js/scripts.js
@@ -0,0 +1,21 @@
+function Button(text) {
+	this.text = text || "Hello";
+}
+    
+
+Button.prototype = {
+    create: function() {
+        var self =this;
+        this.$element = $("<Button>");
+        this.$element.text(this.text);
+        this.$element.click(function(){
+            aler(self.text);
+        })
+        $("Body").append(this.$element);
+}
+}
+
+
+var btn1 = new Button("Hello");
+
+btn1.create();
